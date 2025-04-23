2a8102b28a8eba2a59596e67dbadea2b7bcfedb0
tulobaev <talgattulobaev519>
2025-04-23 20:18:53 +0600

    issue/#25/poppular_button

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -27,6 +27,21 @@ const Category: React.FC<Props> = ({
           <p>{category}</p>
         </div>
       ))}
+
+      <button
+        onClick={() => onSelect("")}
+        className={`${scss.poppular_button} ${
+          selectedCategory === "" ? scss.active : ""
+        }`}
+      >
+        <p>Популярдуу китептер</p>
+        <img
+          width="38"
+          height="38"
+          src="https://img.icons8.com/color/48/books.png"
+          alt="books"
+        />
+      </button>
     </div>
   </div>
 );
