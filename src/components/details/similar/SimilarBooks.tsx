61ca37e58310d9c2bbb9176e400e3f4dcb20a609
tulobaev <talgattulobaev519>
2025-04-27 22:47:13 +0600

    issue/#33/category_logic

--- src/components/details/similar/SimilarBooks.tsx
+++ src/components/details/similar/SimilarBooks.tsx
@@ -1,36 +1,34 @@
 import { FC } from "react";
 import scss from "./SimilarBook.module.scss";
 import { useNavigate } from "react-router-dom";
+import BookCards from "../../../ui/cards/BookCards";
+
+interface Book {
+  id: number;
+  book_name: string;
+  book_image: string | null;
+  description: string;
+  publication_year: number;
+}
 
 const SimilarBooks: FC = () => {
-  const navigate = useNavigate();
-  const books = new Array(20).fill(0).map((_, i) => ({
+  const books: Book[] = new Array(20).fill(0).map((_, i) => ({
     id: i + 1,
-    img: "https://abali.ru/wp-content/uploads/2012/01/staraya_oblozhka_knigi.jpg",
-    author: `Алыкул Осмонов${i + 1}`,
+    book_name: `Книга ${i + 1}`,
+    book_image:
+      "https://abali.ru/wp-content/uploads/2012/01/staraya_oblozhka_knigi.jpg",
     description: `Поэзия о жизни и любви. a a a a a a a a a a aa  aa a aa a a a ${
       i + 1
     }`,
-    year: 2025,
+    publication_year: 2025,
   }));
 
   return (
     <section id={scss.similar}>
       <h1>Окшош китептер</h1>
       <div className={scss.content}>
-        {books.map((book, idx) => (
-          <div
-            onClick={() => navigate("/details")}
-            className={scss.cards}
-            key={idx}
-          >
-            <img src={book.img} alt={book.author} />
-            <div className={scss.text}>
-              <h2>{book.author}</h2>
-              <p>{book.description}</p>
-              <span>{book.year}</span>
-            </div>
-          </div>
+        {books.map((book) => (
+          <BookCards key={book.id} book={book} />
         ))}
       </div>
     </section>
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -4,38 +4,37 @@ import HeroSlider from "../slider/HeroSlider";
 import Popular from "../poppular/Popular";
 import Category from "../category/Category";
 import BookList from "../bookList/BookList";
-import { useAppDispatch, useAppSelector } from "../../store/Store";
+import { useAppSelector } from "../../store/Store";
 import axios from "axios";
-import { setData } from "../../store/slice/DataSlice";
 
-// Категории книг
-const categories = [
-  "Математика, Логика",
-  "Физика, техника",
-  "Кыргыз тили жана адабияты",
-  "Орус тили жана адабияты",
-  "сюда ещё добавим категори....",
-];
-
-const api = import.meta.env.VITE_BASE_URL;
+interface ICategoryType {
+  id: number;
+  category_name: string;
+}
 
 const HomePage: FC = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [category, setCategory] = useState<ICategoryType[]>([]);
   const { data } = useAppSelector((s) => s.data);
-  const dispatch = useAppDispatch();
 
   const filteredBooks = selectedCategory
     ? data?.filter(
         (book) => book.category?.category_name === selectedCategory
       ) || []
     : data || [];
+  console.log(data);
 
-  const fetchData = async () => {
-    const { data } = await axios.get(api);
-    dispatch(setData(data.data));
-  };
   useEffect(() => {
-    fetchData();
+    const fetchCategory = async () => {
+      try {
+        const categoryApi = await axios.get("/api/category/");
+        setCategory(categoryApi.data);
+      } catch (error) {
+        console.log("Ошибка при получении данных:", error);
+      }
+    };
+
+    fetchCategory();
   }, []);
 
   return (
@@ -44,7 +43,7 @@ const HomePage: FC = () => {
         <div className={scss.box}>
           <div className={scss.category_block}>
             <Category
-              categories={categories}
+              categories={category}
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
@@ -54,7 +53,7 @@ const HomePage: FC = () => {
             <HeroSlider />
             <div className={scss.category_block_mobile}>
               <Category
-                categories={categories}
+                categories={category}
                 selectedCategory={selectedCategory}
                 onSelect={setSelectedCategory}
               />
