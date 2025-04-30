2ad984988651be7625fcab288d1d70fa746213ff
tulobaev <talgattulobaev519>
2025-05-01 01:57:16 +0600

    issue/#35/category_api

---
+++ src/features/search/SearchResult.tsx
@@ -0,0 +1,106 @@
+import { FC, useEffect, useState } from "react";
+import scss from "./SearchResult.module.scss";
+import { useGetProductQuery } from "../../store/api/book";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/Store";
+import { useNavigate } from "react-router-dom";
+import { setSearchQuery } from "./SearchSlice";
+import { Pagination, Stack } from "@mui/material";
+import ScrollToTop from "../../components/avtoScroll/AvtoScroll";
+
+const SearchResult: FC = () => {
+  const { data: books = [], isLoading } = useGetProductQuery();
+  const query = useSelector((state: RootState) =>
+    state.search.query.toLowerCase()
+  );
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const filteredBooks = books.filter((item) =>
+    item.book_name.toLowerCase().trim().includes(query.trim().toLowerCase())
+  );
+
+  useEffect(() => {
+    return () => {
+      dispatch(setSearchQuery(""));
+    };
+  }, [dispatch]);
+
+  const [page, setPage] = useState(1);
+  const itemPerPages = 16;
+  const count = Math.ceil(filteredBooks.length / itemPerPages);
+  const currentBooks = filteredBooks.slice(
+    (page - 1) * itemPerPages,
+    page * itemPerPages
+  );
+
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  if (isLoading) return <div className={scss.loading}>Загрузка...</div>;
+
+  return (
+    <section className={scss.SearchResult}>
+      <div className="container">
+        <div className={scss.content}>
+          <div className={scss.btn}>
+            <button onClick={() => navigate("/")} className={scss.back}>
+              ← Артка
+            </button>
+          </div>
+          <div className={scss.title}>
+            <h1>Сиз издеген китептер</h1>
+          </div>
+
+          <div className={scss.booksContainer}>
+            {currentBooks.length > 0 ? (
+              currentBooks.map((item) => (
+                <div
+                  onClick={() => navigate(`/details/${item.id}`)}
+                  key={item.id}
+                  className={scss.cards}
+                >
+                  <img
+                    src={item.book_image || "/images/default-book.jpg"}
+                    alt={`Cover of ${item.book_name}`}
+                    className={scss.bookImage}
+                  />
+
+                  <div className={scss.text}>
+                    <h2>{item.book_name}</h2>
+
+                    <p className={scss.description}>{item.description}</p>
+                    <span className={scss.year}>{item.publication_year}</span>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className={scss.noResults}>Сиз издеген китеп табылган жок</p>
+            )}
+          </div>
+        </div>
+        {books.length > itemPerPages && (
+          <div className={scss.pagination}>
+            <Stack spacing={2}>
+              <Pagination
+                onChange={handlePageChange}
+                count={count}
+                shape="rounded"
+                color="primary"
+                page={page}
+              />
+              <ScrollToTop key={page} />
+            </Stack>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SearchResult;
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api";
+import searchReducer from "../features/search/SearchSlice";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
+    search: searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
